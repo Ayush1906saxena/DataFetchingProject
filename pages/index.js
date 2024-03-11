8c22598915029228ef5824ae65f243da26844573
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-03-11 09:12:38 +0530

    Added static generation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
+import path from "path";
+import fs from "fs/promises";
 function HomePage(props) {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+// Since this code will only run on the server side, we can easily use filesystem here without any vulnerabilities
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy_backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
