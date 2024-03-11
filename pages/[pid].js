f1ee1f296e834ac05552d46d7828484a3d4411f4
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-03-12 00:44:39 +0530

    fetching ids dynamically from dummy data

--- pages/[pid].js
+++ pages/[pid].js
@@ -18,6 +18,14 @@ export default function ProductDetail(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy_backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.pid;
@@ -26,9 +34,7 @@ export async function getStaticProps(context) {
   // getStaticProps runs before the component is rendered,
   // so it supports pre rendering and so the search engines can read your data.
 
-  const filePath = path.join(process.cwd(), "data", "dummy_backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -42,10 +48,14 @@ export async function getStaticProps(context) {
 // Dynamic pages dont just need data, you may also need to know which [id] values will be available.
 // We cannot pre-generate all the possible pages
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
+    paths: pathsWithParams,
     // fallback is important when we have a lot of keys that need to be pregenerated
-    fallback: true,
+    fallback: false,
     // we can set fallback to true and then even the paths not listed can be valid
     // basically can help us pregenerate highly visited pages and postpone generation of less-frequent ones
   };
